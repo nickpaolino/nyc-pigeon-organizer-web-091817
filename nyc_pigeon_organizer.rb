08f177d49490b721688cae42ab049ff428e34473
github username <github email address>
2017-09-13 13:06:21 -0400

    Finished the nyc_pigeon_organizer method and passed the tests

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {}
+  pigeon_names = []
+  # Collect the names of each pigeon
+  data.each do |trait, types|
+    pigeon_names << types.values
+    pigeon_names.flatten!
+    pigeon_names.uniq!
+  end
+  # Create an empty hash for each trait under each name
+  pigeon_names.each do |name|
+    pigeon_data[name] = {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+  end
+  # Iterates down to the lowest level to add the traits under each name
+  data.each do |trait, types|
+    types.each do |type, names|
+      names.each do |name|
+        pigeon_data[name][trait] << type.to_s
+      end
+    end
+  end
+  # Returns the new hash
+  pigeon_data
 end
